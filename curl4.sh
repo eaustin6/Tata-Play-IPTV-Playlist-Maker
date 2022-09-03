002b4c28d7ce7b3c2d2901ba08598216f07dadc3
Mobassar@4u <92091277+Mobassar4u@users.noreply.github.com>
2022-09-03 17:49:17 +0530

    Update curl4.sh

--- curl4.sh
+++ curl4.sh
@@ -5,14 +5,14 @@ NC='\033[0m'
 
 if [[ $OSTYPE == 'linux-android'* && $(echo "$TERMUX_VERSION" | cut -c 3-5) -ge "117" ]]; then
     pkg install git
-    git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater || { rm -rf Tata-Play-IPTV-Playlist-AutoUpdater; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater; }
-    cd Tata-Play-IPTV-Playlist-AutoUpdater;
+    git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-Maker || { rm -rf Tata-Play-IPTV-Playlist-Maker; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-Maker; }
+    cd Tata-Play-IPTV-Playlist-Maker;
     bash main4.sh
 
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
       sudo apt-get install git
-      git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater || { rm -rf Tata-Play-IPTV-Playlist-AutoUpdater; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater; }
-      cd Tata-Play-IPTV-Playlist-AutoUpdater;
+      git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-Maker || { rm -rf Tata-Play-IPTV-Playlist-Maker; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-Maker; }
+      cd Tata-Play-IPTV-Playlist-Maker;
       bash main4.sh
 elif [[ $(echo "$TERMUX_VERSION" | cut -c 3-5) -le "117" ]]; then
     echo -e "Please use Latest Termux release, i.e, from FDroid (https://f-droid.org/en/packages/com.termux/)";
