cf12c8a73199fc00b18c9c0f0d259aed2e796a90
Mobassar@4u <92091277+Mobassar4u@users.noreply.github.com>
2022-09-03 17:18:05 +0530

    Add files via upload

---
+++ curl4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+LOCALDIR=$(pwd)
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [[ $OSTYPE == 'linux-android'* && $(echo "$TERMUX_VERSION" | cut -c 3-5) -ge "117" ]]; then
+    pkg install git
+    git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater || { rm -rf Tata-Play-IPTV-Playlist-AutoUpdater; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater; }
+    cd Tata-Play-IPTV-Playlist-AutoUpdater;
+    bash main4.sh
+
+elif [[ $OSTYPE == 'linux-gnu'* ]]; then
+      sudo apt-get install git
+      git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater || { rm -rf Tata-Play-IPTV-Playlist-AutoUpdater; git clone https://github.com/Mobassar4u/Tata-Play-IPTV-Playlist-AutoUpdater; }
+      cd Tata-Play-IPTV-Playlist-AutoUpdater;
+      bash main4.sh
+elif [[ $(echo "$TERMUX_VERSION" | cut -c 3-5) -le "117" ]]; then
+    echo -e "Please use Latest Termux release, i.e, from FDroid (https://f-droid.org/en/packages/com.termux/)";
+    exit 1;
+else
+    echo "Platform not supported"
+    exit 1;
+fi
